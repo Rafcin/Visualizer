cea81e4358f44e4f79238335e00251bda83e29a1
Rafcin <Rafcin.s@gmail.com>
2022-05-10 16:24:08 -0700

    Updated mac.sh to install brew for users who don't have brew and then sfml because I forgot. [WIP] Max SFML | Xcode CML is bitching with versions 12.0 + 12.1 preventing build

--- install/mac.sh
+++ install/mac.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install cmake
+
 brew install freetype
 
 brew install openssl
 
 brew install libffi
+
+brew install sfml
+
+echo "Finished! Make sure you download Xcode + Xcode cmd line binaries. https://developer.apple.com/download/all/?q=Xcode"
